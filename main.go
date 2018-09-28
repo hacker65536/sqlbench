9f086fc29bf10d90ec7f2ee20e50461f296b24ca
hacker65536 <s.hacker65536@gmail.com>
2018-09-28 09:23:52 +0000

    fix

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	fmt.Println("vim-go")
+}
